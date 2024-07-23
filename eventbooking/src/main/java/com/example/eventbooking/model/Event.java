327f2a7c1184e19d7e41dce30f6b1c7f97a6b7a5
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-07-23 16:45:09 +0100

    add the event module

--- eventbooking/src/main/java/com/example/eventbooking/model/Event.java
+++ eventbooking/src/main/java/com/example/eventbooking/model/Event.java
@@ -1,4 +1,28 @@
 package com.example.eventbooking.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Date date;
+    private String location;
+    private String category;
+    private int capacity;
+    private Double price;
+
+    @OneToMany(mappedBy = "event")
+    private List<Reservation> reservations;
+
 }
